fbf7a4e63753b35ea832b448f8ac4aa06b28388c
IcePeak69 <138380288+IcePeak69@users.noreply.github.com>
2023-07-17 18:18:07 +0200

    Add files via upload

---
+++ typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+    strings : [,'@glock25','wenny#0001','i know this is skidded','hello bitches','creds 2 xeno#0666 c:','wenny runs u','https://kirbsware.xyz :D'],
+    typeSpeed : 35,
+    delaySpeed : 90,
+    showCursor: false,
+    loop : true
+  })
